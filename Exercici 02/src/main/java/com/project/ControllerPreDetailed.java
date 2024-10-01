8356c2f683dae5dd8e754ef2ad87068e3091e201
pablo <pablovicenteroura2005@gmail.com>
2024-10-01 19:25:08 +0200

    commit

--- Exercici 02/src/main/java/com/project/ControllerPreDetailed.java
+++ Exercici 02/src/main/java/com/project/ControllerPreDetailed.java
@@ -8,6 +8,8 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
@@ -15,6 +17,8 @@ import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
 
 import java.io.IOException;
+
+import javax.swing.border.EmptyBorder;
 
 public class ControllerPreDetailed {
 
@@ -29,6 +33,17 @@ public class ControllerPreDetailed {
 
     @FXML
     private VBox vBoxContainer;
+
+    public static String nom;
+    public static String imatge;
+    public static String color;
+    public static String nomVideojoc;
+    public static String any;
+    public static String descripció;
+    public static String tipus;
+    public static String procesador;
+    public static String venudes;
+    public static String data;
 
     @FXML
     public void initialize() {
@@ -60,43 +75,67 @@ public class ControllerPreDetailed {
 
    private void cargarPersonatges() {
     for (Personatge personatge : Main.llistaPersonatges ) {
-        crearPanel(personatge.getNom(), event -> {
+        crearPanel(personatge.getNom(), personatge.getImatge(), event -> {
             System.out.println("Clicked on: " + personatge.getNom());
+            nom = personatge.getNom();
+            imatge = personatge.getImatge();
+            color = personatge.getColor();
+            nomVideojoc = personatge.getNomDelVideojoc();
         });    
     }
    }
 
    private void cargarJocs() {
     for (Joc joc : Main.llistaJocs ) {
-        crearPanel(joc.getNom(), event -> {
+        crearPanel(joc.getNom(), joc.getImagen(), event -> {
             System.out.println("Clicked on: " + joc.getNom());
+            nom = joc.getNom();
+            any = joc.getAny() + "";
+            tipus = joc.getTipo();
+            descripció = joc.getDescripcion();
+            imatge = joc.getImagen();
         });
     }
    }
 
    private void cargarConsoles() {
     for (Consola consola : Main.listaConsoles ) {
-        crearPanel(consola.getNom(), event -> {
+        crearPanel(consola.getNom(), consola.getImagen(), event -> {
             System.out.println("Clicked on: " + consola.getNom());
+            nom = consola.getNom();
+            data = consola.getFechaLanzamiento() + "";
+            procesador = consola.getProcesador();
+            color = consola.getColor();
+            venudes = consola.getUnidadesVendidas() + "";
+            imatge = consola.getImagen();
         });
     }
    }
 
-   private void crearPanel(String nombre, EventHandler<ActionEvent> eventHandler) {
-        Pane panel = new Pane(); // Cambia a un tipo de contenedor que prefieras, como HBox o VBox
-        panel.setPrefHeight(50); // Ajusta la altura del panel según sea necesario
-        panel.setStyle("-fx-background-color: lightgrey; -fx-padding: 10;");
+   private void crearPanel(String nombre, String rutaImagen, EventHandler<ActionEvent> eventHandler) {
+    Pane panel = new Pane(); // Cambia a un tipo de contenedor que prefieras, como HBox o VBox
+    panel.setPrefHeight(80);
+    panel.setMinHeight(80);
+    panel.setStyle("-fx-background-color: white; -fx-padding: 10;");
 
-        Text text = new Text(nombre);
-        text.setFill(Color.BLACK);
+    String path = "/assets/images/" + rutaImagen;
+    Image image = new Image(getClass().getResourceAsStream(path));
 
-        Button button = new Button("Ver Detalles");
-        button.setOnAction(eventHandler);
+    ImageView imageView = new ImageView(image);
+    imageView.setFitWidth(70); // Ajusta el ancho según sea necesario
+    imageView.setFitHeight(70); // Ajusta la altura según sea necesario
+    imageView.setPreserveRatio(true); // Mantiene la proporción de la imagen
 
-        HBox hbox = new HBox(text, button);
-        hbox.setSpacing(10);
-        panel.getChildren().add(hbox);
+    Text text = new Text(nombre);
+    text.setFill(Color.BLACK);
 
-        vBoxContainer.getChildren().add(panel); // Añade el panel al VBox
+    Button button = new Button("Descripció avançada");
+    button.setOnAction(eventHandler);
+
+    HBox hbox = new HBox(imageView, text, button); // Agrega la imagen al HBox
+    hbox.setSpacing(10);
+    panel.getChildren().add(hbox);
+
+    vBoxContainer.getChildren().add(panel); // Añade el panel al VBox
     }
 }
